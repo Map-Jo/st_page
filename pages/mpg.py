7fca70cb5eb06c421cddca8f27665cd422b5e148
Map-Jo <105343395+Map-Jo@users.noreply.github.com>
2022-06-12 23:33:14 +0900

    Update mpg.py

--- pages/mpg.py
+++ pages/mpg.py
@@ -56,61 +56,61 @@ if analysis_type=="Single":
         st.dataframe(trend_data[tcols])
 
     subplots=sidebar.checkbox("Show Subplots", True)
-    if len(trends)>0:
-        fig=trend_data.iplot(kind="line", asFigure=True, xTitle="Date", yTitle="Values",
-                            x="date", y=trends, title=f"{trend_level} Trend of {', '.join(trends)}.", subplots=subplots)
-        st.plotly_chart(fig, use_container_width=False)
+#     if len(trends)>0:
+#         fig=trend_data.iplot(kind="line", asFigure=True, xTitle="Date", yTitle="Values",
+#                             x="date", y=trends, title=f"{trend_level} Trend of {', '.join(trends)}.", subplots=subplots)
+#         st.plotly_chart(fig, use_container_width=False)
 
-if analysis_type=="Multiple":
-    selected = sidebar.multiselect("Select Locations ", locations)
-    st.markdown(f"## Selected Locations: {', '.join(selected)}")
-    show_data = sidebar.checkbox("Show Data")
-    trend_level = sidebar.selectbox("Trend Level", ["Daily", "Weekly", "Monthly", "Quarterly", "Yearly"])
-    st.markdown(f"### Currently Selected {trend_level}")
+# if analysis_type=="Multiple":
+#     selected = sidebar.multiselect("Select Locations ", locations)
+#     st.markdown(f"## Selected Locations: {', '.join(selected)}")
+#     show_data = sidebar.checkbox("Show Data")
+#     trend_level = sidebar.selectbox("Trend Level", ["Daily", "Weekly", "Monthly", "Quarterly", "Yearly"])
+#     st.markdown(f"### Currently Selected {trend_level}")
 
-    trend_kwds = {"Daily": "1D", "Weekly": "1W", "Monthly": "1M", "Quarterly": "1Q", "Yearly": "1Y"}
+#     trend_kwds = {"Daily": "1D", "Weekly": "1W", "Monthly": "1M", "Quarterly": "1Q", "Yearly": "1Y"}
 
-    trend_data = data.query(f"location in {selected}").\
-        groupby(["location", pd.Grouper(key="date", 
-        freq=trend_kwds[trend_level])]).aggregate(new_cases=("new_cases", "sum"),
-        new_deaths = ("new_deaths", "sum"),
-        new_vaccinations = ("new_vaccinations", "sum"),
-        new_tests = ("new_tests", "sum")).reset_index()
+#     trend_data = data.query(f"location in {selected}").\
+#         groupby(["location", pd.Grouper(key="date", 
+#         freq=trend_kwds[trend_level])]).aggregate(new_cases=("new_cases", "sum"),
+#         new_deaths = ("new_deaths", "sum"),
+#         new_vaccinations = ("new_vaccinations", "sum"),
+#         new_tests = ("new_tests", "sum")).reset_index()
 
-    trend_data["date"] = trend_data.date.dt.date
+#     trend_data["date"] = trend_data.date.dt.date
 
-    new_cases = sidebar.checkbox("New Cases")
-    new_deaths = sidebar.checkbox("New Deaths")
-    new_vaccinations = sidebar.checkbox("New Vaccinations")
-    new_tests = sidebar.checkbox("New Tests")
+#     new_cases = sidebar.checkbox("New Cases")
+#     new_deaths = sidebar.checkbox("New Deaths")
+#     new_vaccinations = sidebar.checkbox("New Vaccinations")
+#     new_tests = sidebar.checkbox("New Tests")
 
-    lines = [new_cases, new_deaths, new_vaccinations, new_tests]
-    line_cols = ["new_cases", "new_deaths", "new_vaccinations", "new_tests"]
-    trends = [c[1] for c in zip(lines,line_cols) if c[0]==True]
+#     lines = [new_cases, new_deaths, new_vaccinations, new_tests]
+#     line_cols = ["new_cases", "new_deaths", "new_vaccinations", "new_tests"]
+#     trends = [c[1] for c in zip(lines,line_cols) if c[0]==True]
 
-    ndf = pd.DataFrame(data=trend_data.date.unique(),columns=["date"])
+#     ndf = pd.DataFrame(data=trend_data.date.unique(),columns=["date"])
 
-    for s in selected:
-        new_cols = ["date"]+[f"{s}_{c}" for c in line_cols]
-        tdf = trend_data.query(f"location=='{s}'")
-        tdf.drop("location", axis=1, inplace=True)
-        tdf.columns=new_cols
-        ndf=ndf.merge(tdf,on="date",how="inner")
+#     for s in selected:
+#         new_cols = ["date"]+[f"{s}_{c}" for c in line_cols]
+#         tdf = trend_data.query(f"location=='{s}'")
+#         tdf.drop("location", axis=1, inplace=True)
+#         tdf.columns=new_cols
+#         ndf=ndf.merge(tdf,on="date",how="inner")
 
-    if show_data:
-        if len(ndf)>0:
-            st.dataframe(ndf)
-        else:
-            st.markdown("Empty Dataframe")
+#     if show_data:
+#         if len(ndf)>0:
+#             st.dataframe(ndf)
+#         else:
+#             st.markdown("Empty Dataframe")
 
-    new_trends = []
-    for c in trends:
-        new_trends.extend([f"{s}_{c}" for s in selected])
+#     new_trends = []
+#     for c in trends:
+#         new_trends.extend([f"{s}_{c}" for s in selected])
 
-    subplots=sidebar.checkbox("Show Subplots", True)
-    if len(trends)>0:
-        st.markdown("### Trend of Selected Locations")
+#     subplots=sidebar.checkbox("Show Subplots", True)
+#     if len(trends)>0:
+#         st.markdown("### Trend of Selected Locations")
 
-        fig=ndf.iplot(kind="line", asFigure=True, xTitle="Date", yTitle="Values",
-                            x="date", y=new_trends, title=f"{trend_level} Trend of {', '.join(trends)}.", subplots=subplots)
-        st.plotly_chart(fig, use_container_width=False)
+#         fig=ndf.iplot(kind="line", asFigure=True, xTitle="Date", yTitle="Values",
+#                             x="date", y=new_trends, title=f"{trend_level} Trend of {', '.join(trends)}.", subplots=subplots)
+#         st.plotly_chart(fig, use_container_width=False)
